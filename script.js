d981313ff69d37fccf523495338d9ea465488365
pahiy <ayazdoruck@gmail.com>
2025-10-23 20:36:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'https://pahiy-ai-production.up.railway.app';
+const BACKEND_URL = 'web-production-ba19b.up.railway.app';
 
 let conversationHistory = [];
 let currentSessionId = generateSessionId();
